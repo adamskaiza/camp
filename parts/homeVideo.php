598c7794e20700033b3d4b3e6e18d3570dd23c51
olekk <teletubis28@gmail.com>
2022-10-21 09:48:25 +0200

    homeVideo WiP

---
+++ parts/homeVideo.php
@@ -0,0 +1,11 @@
+    <div class="homeVideo">
+        <div class="container">
+            <img src="<?php echo get_template_directory_uri(); ?>/src/img/logo-CAMP.png" alt="Camp Logo">
+        </div>
+        <div class="iframe-container">
+            <?php $videoId='4KhJtE04dt4'; ?>
+            <iframe width="auto" height="auto"
+                src="https://www.youtube.com/embed/<?php echo $videoId; ?>?autoplay=1&mute=1&modestbranding=1&autohide=1&showinfo=0&controls=0&version=3&loop=1&playlist=<?php echo $videoId; ?>"
+                title="Women in Tech Camp 2021" frameborder="0" allow="autoplay; picture-in-picture"></iframe>
+        </div>
+    </div>
