3daa5c1972b335fc92b67a9895e5aa74db29f2d3
Iza Adamska <i.b.adamska@gmail.com>
2022-10-25 12:09:16 +0200

    footer styles

--- footer.php
+++ footer.php
@@ -12,7 +12,19 @@
 ?>
 
 <footer id="colophon" class="site-footer">
-
+    <section class="contacts">
+        <?php $contacts = get_field('footer_contacts', 'options');?>
+        <ol class="contacts__list container">
+            <?php foreach($contacts as $contact) :?>
+            <li>
+                <?php get_template_part('svg/hex_4');?>
+                <h3><?php echo $contact['name'];?></h3>
+                <?php echo $contact['content'];?>
+                <?php echo wp_get_attachment_image($contact['avatar'], 'thumbnail');?>
+            </li>
+            <?php endforeach;?>
+        </ol>
+    </section>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
