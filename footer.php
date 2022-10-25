e318afa276ef4ad580a34c3b5373dd7d36da9f63
Iza Adamska <i.b.adamska@gmail.com>
2022-10-25 21:54:37 +0200

    contacts more responsive

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 
 <footer id="colophon" class="footer">
     <section class="contacts">
+        <h2 class="contacts__heading container">KONTAKT</h2>
         <?php $contacts = get_field('footer_contacts', 'options');?>
         <ol class="contacts__list container">
             <?php foreach($contacts as $contact) :?>
