2ab7473bd063d0148abd10e59351401fac70c967
Iza Adamska <i.b.adamska@gmail.com>
2022-10-28 15:13:31 +0200

    program tabs and accordion already done

---
+++ parts/program-acc-cart.php
@@ -0,0 +1,22 @@
+<?php $acc_carts = $args['acc_cart'];
+$cartCount = 1;
+?>
+
+<section class="acc">
+    <form class="acc__container">
+        <?php foreach($acc_carts as $acc_cart):?>
+
+        <section class="acc__cart">
+            <input type="checkbox" id="<?php echo $id;?>-<?php echo $cartCount;?>" name="cb"
+                value="<?php echo $acc_cart['acc_title'];?>" <?php echo $cartCount == 1 ? 'checked' : '';?>>
+            <label class="acc__cart-label"
+                for="<?php echo $id;?>-<?php echo $cartCount;?>"><?php echo $acc_cart['acc_title'];?></label>
+            <article class="acc__cart-content">
+                <?php echo $acc_cart['acc_content'];?> </article>
+        </section>
+        <?php $cartCount++;?>
+        <?php endforeach;?>
+
+
+    </form>
+</section>
--- parts/program-tabs.php
+++ parts/program-tabs.php
@@ -2,16 +2,18 @@
 <section class="tabs program-tabs">
     <header class="program-tabs__header container">
         <h1 class="h2">PROGRAM</h1>
-        <ul class="program-tabs__nav tabs-nav">
-            <?php $link_counter = 1;?>
-            <?php foreach ($program as $item) : ?>
-            <li class="program-tabs__item">
-                <button value="<?php echo $item['heading'];?>"
-                    class="program-tabs__link tabs-link <?php echo $link_counter === 1 ? 'tabs-link--active' : '';?>"><?php echo $item['heading'];?></button>
-            </li>
-            <?php $link_counter++ ;?>
-            <?php endforeach;?>
-        </ul>
+        <nav class="program-tabs__nav tabs-nav">
+            <ul>
+                <?php $link_counter = 1;?>
+                <?php foreach ($program as $item) : ?>
+                <li class="program-tabs__item">
+                    <button value="<?php echo $item['heading'];?>"
+                        class="program-tabs__link tabs-link <?php echo $link_counter === 1 ? 'tabs-link--active' : '';?>"><?php echo $item['heading'];?></button>
+                </li>
+                <?php $link_counter++ ;?>
+                <?php endforeach;?>
+            </ul>
+        </nav>
     </header>
 
 
@@ -27,7 +29,17 @@
                 style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/src/img/hex_dzien_' . $tab_content_counter . '.png';?>)">
                 <div class="program-tabs__line-wrapper container">
                     <?php echo $item['title'];?>
+                    <p>Dzień <?php echo $tab_content_counter;?></p>
+                    <section class="program__program program">
+                        <?php foreach ($item['components'] as $program_component): ?>
 
+                        <?php get_template_part('parts/program-component', null, [
+                            'timing' =>  $program_component['timing'],
+                            'title' =>  $program_component['component_title'],
+                            'component_content' => $program_component['component_content'],
+                            ]);?>
+                        <?php endforeach;?>
+                    </section>
                 </div>
 
             </article>
