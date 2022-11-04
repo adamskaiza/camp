8740fddf682b2fdee98127d6f0a3aa96568fcb83
olekk <aleksander.ciesla@protonmail.com>
2022-11-04 01:12:13 +0100

    hexProgram RWD fix

--- parts/hexProgram.php
+++ parts/hexProgram.php
@@ -1,17 +1,18 @@
-<section class="hexProgram container" id="o-programie">
+<section class="hexProgram" id="o-programie">
     <?php $data=get_field('hexprogram'); ?>
 
-    <div class="hexProgram__title">
-        <div class="hexProgram__title_title">
-            <?php echo $data['title'] ?>
+    <div class="container">
+        <div class="hexProgram__title">
+            <div class="hexProgram__title_title">
+                <?php echo $data['title'] ?>
+            </div>
+            <div class="hexProgram__title_subtitle">
+                <?php echo $data['subtitle'] ?>
+            </div>
         </div>
-        <div class="hexProgram__title_subtitle">
-            <?php echo $data['subtitle'] ?>
-        </div>
-    </div>
-    
-    <div class="hexProgram__tiles">
-        <?php foreach ($data['tiles'] as $key => $value): ?>
+
+        <div class="hexProgram__tiles">
+            <?php foreach ($data['tiles'] as $key => $value): ?>
             <div class="hexProgram__tiles_hex">
 
                 <?php echo wp_get_attachment_image( $value['icon']) ?>
@@ -22,15 +23,16 @@
                 <div class="hexProgram__tiles_hex--content">
                     <?php echo $value['content'] ?>
                 </div>
-                
+
                 <?php echo wp_get_attachment_image( $value['sideimage'], null, null, 
                 ["class"=>'hexProgram__tiles_hex--sideimage']) ?>
-                
+
                 <?php echo wp_get_attachment_image($value['bgimage'], null, null, 
                 ["class"=>'hexProgram__tiles_hex--bg']) ?>
 
             </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
 
 </section>
