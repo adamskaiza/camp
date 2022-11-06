2d3a0d2ce42f950e32db5517c1ac87561c1ad89c
olekk <aleksander.ciesla@protonmail.com>
2022-11-06 21:52:43 +0100

    hex rwd

--- parts/homeVideo.php
+++ parts/homeVideo.php
@@ -1,22 +1,36 @@
 <section class="homeVideo" id="homeVideo">
+    <?php $data = get_field('homevideo'); ?>
     <?php /* ?>
     <div class="video-container">
         <video muted autoplay="autoplay" loop>
-            <source src="https://perspektywy.org/womenintechcamp/wp-content/uploads/Women-in-Tech-Camp-2021_2.mp4" type="video/mp4">
+            <source src="https://perspektywy.org/womenintechcamp/wp-content/uploads/Women-in-Tech-Camp-2021_2.mp4"
+                type="video/mp4">
         </video>
         <video autoplay loop muted playsinline onloadeddata="this.muted=true;this.play();">
-            <source src="https://perspektywy.org/womenintechcamp/wp-content/uploads/Women-in-Tech-Camp-2021_2.mp4" type="video/mp4">
+            <source src="https://perspektywy.org/womenintechcamp/wp-content/uploads/Women-in-Tech-Camp-2021_2.mp4"
+                type="video/mp4">
         </video>
     </div>
 
-    <div class="homeVideo__gif">
-        <?php echo wp_get_attachment_image( $value['gif'], 'full') ?>
-    </div>
     <?php */ ?>
+    <?php 
+        if(isset($data['gif'])) :
+    ?>
+
+    <div class="homeVideo__gif">
+        <?php echo wp_get_attachment_image( $data['gif'], 'full') ?>
+    </div>
+
+    <?php
+        elseif(isset($data['videoid'])) :
+    ?>
     <div class="iframe-container">
-        <?php $videoId=get_field('homevideo')['videoid']; ?>
+        <?php $videoId= $data['videoid']; ?>
         <iframe
             src="https://www.youtube.com/embed/<?php echo $videoId; ?>?autoplay=1&mute=1&modestbranding=1&autohide=1&showinfo=0&controls=0&version=3&loop=1&playlist=<?php echo $videoId; ?>"
             title="Women in Tech Camp 2021" frameborder="0" allow="autoplay; picture-in-picture"></iframe>
     </div>
+    <?php
+        endif;
+    ?>
 </section>
