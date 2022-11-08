319a32d06f96e2cd2fe629a28be6af3e7054859f
Iza Adamska <i.b.adamska@gmail.com>
2022-11-08 15:07:54 +0100

    crew item mod

--- parts/crew-item.php
+++ parts/crew-item.php
@@ -1,15 +1,17 @@
 <?php $role = get_terms('role');?>
 <li class="crew-list__item crew-item <?php echo $args['modifier'] ?? null;?>">
-    <a href="<?php echo the_permalink();?>">
+    <a href="<?php echo the_permalink();?>"></a>
+    <article class="crew-item__block">
         <?php camp_post_thumbnail();?>
         <?php if(has_post_thumbnail()):?>
         <?php else : ?>
         <img src="<?php echo get_stylesheet_directory_uri() . '/src/img/avatar.jpg' ;?>" alt="avatar">
         <?php endif;?>
-        <div>
+        <article>
             <h3><?php the_title();?></h3>
             <?php the_excerpt();?>
-            <p class="crew-item__firm"><?php echo get_first_paragraph();?></p>
-        </div>
-    </a>
+        </article>
+    </article>
+
+    <p class="crew-item__firm"><?php echo get_first_paragraph();?></p>
 </li>
