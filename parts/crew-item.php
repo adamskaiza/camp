43dc0f6bafab2b6196568cbafd8e138f6e642a1d
Iza Adamska <i.b.adamska@gmail.com>
2022-11-14 12:37:09 +0100

    hover on crew item

--- parts/crew-item.php
+++ parts/crew-item.php
@@ -1,14 +1,16 @@
 <?php $role = get_terms('role');?>
 <li class="crew-list__item crew-item <?php echo $args['modifier'] ?? null;?>">
-    <a href="<?php echo the_permalink();?>"></a>
     <article class="crew-item__block">
         <?php camp_post_thumbnail();?>
         <?php if(has_post_thumbnail()):?>
         <?php else : ?>
-        <img src="<?php echo get_stylesheet_directory_uri() . '/src/img/avatar.jpg' ;?>" alt="avatar">
+        <div class="post-thumbnail">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/src/img/avatar.jpg' ;?>" alt="avatar">
+        </div>
         <?php endif;?>
         <article>
-            <h3><?php the_title();?></h3>
+            <h3> <a href="<?php echo the_permalink();?>">
+                    <?php the_title();?></a></h3>
             <?php the_excerpt();?>
             <p class="crew-item__firm--acc"><?php echo get_first_paragraph();?></p>
 
